bf6cab24bf36547fb970ce8b460029f0746f31ac
user.B <1404369418@qq.com>
2020-06-01 00:38:42 +0800

    new add noname.java

---
+++ Noname1.java
@@ -0,0 +1,9 @@
+class demo001 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello china!");
+		System.out.println("Hello jiangsu!");
+	}
+}
